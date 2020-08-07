79c9283e4798e64d91d30408fd809b6640efe998
abhishekgpta <gptaabhishek93@gmail.com>
2020-08-07 10:47:46 +0530

    updated drag and drop

--- src/components/curriculum.js
+++ src/components/curriculum.js
@@ -8,10 +8,9 @@ export default props =>{
     const {dataset={}, setData} = props
     let [selectedStandard,setSelectedStandard] = useState('');
     let [selectedInputData,setSelectedInputData] = useState('');
-    let [standardDragged, setStandardDragged] = useState({})
 
     const handleAddRow = ()=>{
-        const id = new Date().getTime();
+        const id = new Date().getTime().toString();
         let newDataSet = {
             ...dataset,
             [id]:{
@@ -72,6 +71,7 @@ export default props =>{
         }
 
     }
+    //TODO Break left and right indent to seperate functions
     const handleIndent =(type,id)=>{
         const newDataSet = {...dataset};
         const stdIds = Object.keys(newDataSet);
@@ -110,7 +110,7 @@ export default props =>{
                     shiftedStd.indentation++;
                     const oldParentStdID = shiftedStd.parentID;
                     //removing child from old parent
-                    newDataSet[oldParentStdID].children = newDataSet[oldParentStdID].children.filter((childids)=>childids !== id); 
+                    newDataSet[oldParentStdID].children = newDataSet[oldParentStdID].children.filter((childids)=>childids.toString() !== id.toString()); 
                     //updating parent of shifted standard
                     shiftedStd.parentID = stdIds[index-1];
                     newDataSet[stdIds[index-1]].children.push(id);
@@ -148,36 +148,108 @@ export default props =>{
                 for(let ids of allChildrenArray){
                     newDataSet[ids].indentation--;
                 }
-                //add new standard children lower than shifted standard
-                let lowerStdID = stdIds[index+1];
-                if(newDataSet[lowerStdID]){
-                    const oldParentID = newDataSet[lowerStdID].parentID;
-                    newDataSet[oldParentID].children = newDataSet[oldParentID].children.filter((childId)=>childId.toString() !==lowerStdID.toString());
-                    newDataSet[lowerStdID].parentID = id;
-                    if(newDataSet[id].children.indexOf(lowerStdID)=== -1){
-                        newDataSet[id].children.push(lowerStdID);
-                    }
-                }
             }
         } 
         setData(newDataSet);
     }
     const handleDragStart=(e, standardId)=>{
-        setStandardDragged({standardId})
         e.dataTransfer.setData("id", standardId);
         const ele = document.getElementById(`app_course_${standardId}`);
         ele.style.backgroundColor="aliceblue";
         e.dataTransfer.setDragImage(ele,0,0);
     }
     const handleDrop = (ev, standardId)=>{
-        const ele = document.getElementById(`app_course_${standardId}`);
+        const draggedStandardID = ev.dataTransfer.getData("id");
+        const ele = document.getElementById(`app_course_${draggedStandardID}`);
         ele.style.backgroundColor="white";
+        let dataSetValues = Object.values(dataset);
+        let dataSetKeys = Object.keys(dataset);
+        let draggedStandardObject = dataSetValues.filter((stdObject)=>stdObject.id.toString() === draggedStandardID.toString())[0] || {};
+        let newParentObject = dataSetValues.filter((stdObject)=>stdObject.id.toString() === standardId.toString())[0] || {};
         /*
          * To drag and drop standard from one sub tree to another subtree
-         * 1. Indentation of selected standard should not change
-         * 2. update parent ref for selected standard
-         * 3. check for parent indentation before and after drag n drop 
+         * 1. To move standard Tree drag selected standard tree and drop it in the parent of destination standard tree 
+         * 2. Indentation of selected standard should not change
+         * 3. update parent ref for selected standard
+         * 4. check for parent indentation before and after drag n drop 
+         * 5. move selected standard and all its childeren
          */
+        if((Object.keys(newParentObject).length > 0) && Object.keys(draggedStandardObject).length > 0){
+            if((draggedStandardObject.indentation > newParentObject.indentation) && (Math.abs(draggedStandardObject.indentation - newParentObject.indentation)===1)){
+                const oldParentId = draggedStandardObject.parentID;
+                if(oldParentId){
+                    draggedStandardObject.parentID = standardId;
+                    newParentObject.children.push(draggedStandardID);
+                    let newDataSet = {};
+                    //dragging from source downward direction
+                    if(dataSetKeys.indexOf(standardId) > dataSetKeys.indexOf(draggedStandardID)){
+                        //from top to source
+                        for (const dataValue of dataSetValues.slice(0,dataSetKeys.indexOf(draggedStandardID))) {
+                            if(dataValue.id === oldParentId){
+                                dataValue.children = dataValue.children.filter((childIds)=>childIds !== draggedStandardID);
+                            }
+                            newDataSet[dataValue.id] = {...dataValue};
+                        }
+                        let allChildrenDraggedDatasetArray=[];
+                        getAllChildrens(draggedStandardID, allChildrenDraggedDatasetArray);
+                        let allChildrenNewParenArray=[];
+                        getAllChildrens(standardId, allChildrenNewParenArray);
+                        //from source to new parent
+                        for (const dataValue of dataSetValues.slice(dataSetKeys.indexOf(draggedStandardID),dataSetKeys.indexOf(standardId))) {
+                            if(allChildrenDraggedDatasetArray.indexOf(dataValue.id) === -1 && dataValue.id!==draggedStandardID){
+                                newDataSet[dataValue.id] = {...dataValue};
+                            }
+                        }
+                        newDataSet[standardId] = {...dataset[standardId]};
+                        for (const iterator of allChildrenNewParenArray) {
+                            newDataSet[iterator] = {...dataset[iterator]};
+                        }
+                        newDataSet[draggedStandardID] = {...dataset[draggedStandardID]};
+                        newDataSet[draggedStandardID].parentID = standardId;
+
+                        for (const iterator of allChildrenDraggedDatasetArray) {
+                            newDataSet[iterator] = {...dataset[iterator]};
+                        }
+                        //from new parent to rest of the tree
+                        for (const dataValue of dataSetValues.slice(dataSetKeys.indexOf(standardId)+1)) {
+                            if(!newDataSet[dataValue.id]){
+                                newDataSet[dataValue.id] = {...dataValue};
+                            }
+                        }
+                        setData(newDataSet)
+                    }
+                    else{
+                        //from top to new parent
+                        for (const dataValue of dataSetValues.slice(0, dataSetKeys.indexOf(standardId))) {
+                            newDataSet[dataValue.id] = {...dataValue};
+                        }
+                        newDataSet[standardId] = {...dataSetValues.filter((val)=>val.id === standardId)[0]}
+                        let allChildrenNewParenArray=[];
+                        getAllChildrens(standardId, allChildrenNewParenArray);
+                        for (const iterator of allChildrenNewParenArray) {
+                            newDataSet[iterator] = {...dataset[iterator]};
+                        }
+                        let allChildrenDraggedDatasetArray=[];
+                        getAllChildrens(draggedStandardID, allChildrenDraggedDatasetArray);
+                        newDataSet[draggedStandardID] = {...dataset[draggedStandardID]};
+                        newDataSet[draggedStandardID].parentID = standardId;
+                        for (const iterator of allChildrenDraggedDatasetArray) {
+                            newDataSet[iterator] = {...dataset[iterator]};
+                        }
+                        //new parent to souce
+                        for (const dataValue of dataSetValues.slice(dataSetKeys.indexOf(standardId)+1)) {
+                            if(!newDataSet[dataSetValues.id]){
+                                if(dataValue.id === oldParentId){
+                                    dataValue.children = [...dataValue.children.filter((childIds)=>childIds !== draggedStandardID)];
+                                }
+                                newDataSet[dataValue.id] = {...dataValue};
+                            }
+                        }
+                    }
+                    setData(newDataSet)
+                } 
+            }
+        }
     }
     useEffect(() => {
 		if (selectedStandard && document.querySelector(".app__standard__input")) {
